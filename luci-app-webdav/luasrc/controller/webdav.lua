8e3c153addd8da8791b0bc7e911740664727bcb1
clg <chenglanguo@ritchiebros.com>
2024-12-24 01:05:45 +0800

    test

--- luci-app-webdav/luasrc/controller/webdav.lua
+++ luci-app-webdav/luasrc/controller/webdav.lua
@@ -7,4 +7,5 @@ function index()
 
     entry({"admin", "services", "webdav"}, cbi("webdav"), _("WebDAV Server"), 10).dependent = true
     entry({"admin", "services", "webdav", "status"}, template("webdav/webdav_status"), _("Status"), 20).leaf = true
+    entry({"admin", "services", "webdav", "link"}, template("webdav/webdav_link"), _("WebDAV Link"), 30).leaf = true
 end
