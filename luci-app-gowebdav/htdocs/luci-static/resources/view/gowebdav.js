c941c6ae42f00d93490080b0a0adb93c9bb77813
clg <chenglanguo@ritchiebros.com>
2024-12-28 20:43:59 +0800

    remote to fix error

--- luci-app-gowebdav/htdocs/luci-static/resources/view/gowebdav.js
+++ luci-app-gowebdav/htdocs/luci-static/resources/view/gowebdav.js
@@ -109,9 +109,6 @@ return view.extend({
 		o.datatype = 'file';
 		o.depends('use_https', 1);
 
-		o = s.option(DummyValue,"opennewwindow",translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Download Reg File\" onclick=\"window.open('https://raw.githubusercontent.com/1715173329/gowebdav/master/allow_http.reg')\" />"))
-		o.description = translate("Windows doesn't allow HTTP auth by default, you need to import this reg key to enable it (Reboot needed).")
-
 		return m.render();
 	}
 });
